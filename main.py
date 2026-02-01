333a635c036574768bfc62b4ddead5bfc7d42d8d
Francesco-Nasoni <f.nasoni@outlook.it>
2026-02-01 17:13:04 +0100

    Created function solve_shrodinger, generalized for any V_eff

--- main.py
+++ main.py
@@ -13,7 +13,17 @@ from scipy.integrate import simpson
 import matplotlib.pyplot as plt
 
 
-def F(E, r, u_r, V_eff=0):
+class RadialGrid:
+    def __init__(self, r_min=1e-6, r_max=10.0, h=1e-3):
+        self.r_min = r_min
+        self.r_max = r_max
+        self.h = h
+        self.r = np.arange(r_min, r_max, h)
+        self.N = len(self.r)
+        self.r_rev = np.flip(self.r)
+
+
+def F(E, V_eff, r, u_r):
     return (-4 / r - 2 * E + 2 * V_eff) * u_r
 
 
@@ -51,62 +61,64 @@ def poisson_integrate(u_r, line_grid):
     return V_hartree
 
 
-class RadialGrid:
-    def __init__(self, r_min=1e-6, r_max=10.0, h=1e-3):
-        self.r_min = r_min
-        self.r_max = r_max
-        self.h = h
-        self.r = np.arange(r_min, r_max, h)
-        self.N = len(self.r)
-        self.r_rev = np.flip(self.r)
+def solve_shrodinger(grid, Z, V_eff, E_bounds, E_rough_step):
+    h = grid.h
+    u_rmax = grid.r_max * np.exp(-Z * grid.r_max)
+    u_rmax_h = (grid.r_max - grid.h) * np.exp(-Z * grid.r_max + Z * h)
+
+    # Look for two points where u(0) chenges sign for the bisect method
+    u_0 = verlet_integrate_1D(
+        lambda r, ur: F(E_bounds[0], V_eff, r, ur), u_rmax, u_rmax_h, grid.r_rev
+    )
+    u_prev = u_0
+
+    for E_test in np.arange(
+        E_bounds[0] + E_rough_step, E_bounds[1], E_rough_step
+    ):
+        u_i = verlet_integrate_1D(
+            lambda r, ur: F(E_test, V_eff, r, ur), u_rmax, u_rmax_h, grid.r_rev
+        )
+        if np.sign(u_prev[-1]) != np.sign(u_i[-1]):
+            E_bisect_range = np.array([E_test, E_test - E_rough_step])
+            break
+        else:
+            u_prev = u_i
+
+    # Proper bisect method
+    E_root = bisect(
+        lambda e: verlet_integrate_1D(
+            lambda r, ur: F(e, V_eff, r, ur), u_rmax, u_rmax_h, grid.r_rev
+        )[-1],
+        E_bisect_range[0],
+        E_bisect_range[1],
+        xtol=grid.h,
+    )
+
+    # NOTE: np.flip is necessary since we are integrating backwords r_max -> 0
+    u_int = np.flip(
+        verlet_integrate_1D(
+            lambda r, ur: F(E_root, V_eff, r, ur), u_rmax, u_rmax_h, grid.r_rev
+        )
+    )
+
+    norm = get_norm_factor(u_int, grid.r)
+
+    return u_int / norm, E_root
 
 
-# --- PARAMETERS ---
-E_thr = 1e-3
+# --- GENERAL PARAMETERS --- #
+Z = 2
+# E_thr = 1e-3
 
-# --- GRID ---
+# --- GRID --- #
 grid = RadialGrid(r_min=1e-6, r_max=10.0, h=1e-3)
 
-# --- PARAMETERS FOR BISECT-PRELIMINARY TO BISECT
-E_range_search = (-3, 0)
-E_step = 0.05
+# --- PARAMETERS FOR BISECT-PRELIMINARY TO BISECT --- #
+E_search_range = (-3, 0)
+E_rough_step = 0.05
 
-u_rmax = grid.r_max * np.exp(-2 * grid.r_max)
-u_rmax_h = (grid.r_max - grid.h) * np.exp(-2 * grid.r_max + grid.h)
-
-# Look for two points where u_int chenges sign since we want the value of E
-# for which u_int=0
-
-u_0 = verlet_integrate_1D(
-    lambda r, ur: F(E_range_search[0], r, ur), u_rmax, u_rmax_h, grid.r_rev
-)
-u_prev = u_0
-
-for E_test in np.arange(E_range_search[0] + E_step, E_range_search[1], E_step):
-    u_i = verlet_integrate_1D(
-        lambda r, ur: F(E_test, r, ur), u_rmax, u_rmax_h, grid.r_rev
-    )
-    if np.sign(u_prev[-1]) != np.sign(u_i[-1]):
-        E_bisect_range = np.array([E_test, E_test - E_step])
-        break
-    else:
-        u_prev = u_i
-
-E_root = bisect(
-    lambda e: verlet_integrate_1D(
-        lambda r, ur: F(e, r, ur), u_rmax, u_rmax_h, grid.r_rev
-    )[-1],
-    E_bisect_range[0],
-    E_bisect_range[1],
-    xtol=grid.h,
-)
-
-# NOTE: np.flip is necessary since we are integrating backwords r_max -> 0
-u_int = np.flip(
-    verlet_integrate_1D(lambda r, ur: F(E_root, r, ur), u_rmax, u_rmax_h, grid.r_rev)
-)
-
-print(f"E_root: {E_root}")
+# --- FIRST CALCULATION WITH V_eff=0 --- #
+u_int, E_root = solve_shrodinger(grid, Z, 0, E_search_range, E_rough_step)
 
 plt.figure(figsize=(10, 6))
 plt.plot(grid.r, u_int)
